8a914366deda57a9fc110aa7199bf42dd8362df1
thalita coelho bastos <thalita.bastos94@gmail.com>
2024-07-14 13:17:22 -0300

    configurando projeto

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    return res.json('Api está OK')
+})
+
+app.listen(3000);
